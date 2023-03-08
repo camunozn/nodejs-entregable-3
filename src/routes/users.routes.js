a412a3a79868e055c4eff46163fb29739951d59f
César Muñoz <camunozn89@gmail.com>
2023-03-07 22:20:20 -0500

    Fix users.routes bug

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { createUser } = require('../controllers/users.controllers');
+const { createUser } = require('../controllers/users.controller');
 
 const router = Router();
 
